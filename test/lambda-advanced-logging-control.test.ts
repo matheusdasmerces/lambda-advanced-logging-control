45089131a99d795ebf9396a0d3ebb720a7a84190
matheusdasmerces <matheusdasmerces@postnl.nl>
2025-02-09 17:16:47 +0100

    Initial commit

---
+++ test/lambda-advanced-logging-control.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaAdvancedLoggingControl from '../lib/lambda-advanced-logging-control-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-advanced-logging-control-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaAdvancedLoggingControl.LambdaAdvancedLoggingControlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
